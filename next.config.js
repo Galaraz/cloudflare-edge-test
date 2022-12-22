3bf2b597e159714af06c4e2ba150b608f35c6e2c
galaraz <jvictor.si.2013@gmail.com>
2022-12-22 11:53:30 -0400

    denovo

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
-  experimental: {
-       runtime: 'experimental-edge',
-  },
+ 
   reactStrictMode: true,
   swcMinify: false,
   images: {
